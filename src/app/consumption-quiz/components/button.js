f8643f742a7de951ef6245729449dd0a2e49e5fc
Jakub Jachowicz <kubajachowicz39@gmail.com>
2025-05-31 16:44:58 +0200

    guziczek dodany

--- src/app/consumption-quiz/components/button.js
+++ src/app/consumption-quiz/components/button.js
@@ -3,9 +3,15 @@ import { AiFillPlayCircle } from "react-icons/ai";
 
 function StartButton()
 {
-    return <div className="w-30">
-        <AiFillPlayCircle />
-        <p>Rozpocznij</p>
+    return <div className="w-60 m-4
+                            border-[var(--hunter-green)]
+                            rounded-3xl border-4 p-4
+                            bg-[var(--yellow-green)]
+                            flex-row
+                            flex
+                            place-content-center">
+        <AiFillPlayCircle className="text-xl mt-2 mx-1"/>
+        <p className="text-2xl">Rozpocznij</p>
     </div>
 }
 
--- src/app/consumption-quiz/page.js
+++ src/app/consumption-quiz/page.js
@@ -3,8 +3,8 @@ import StartButton from "@/app/consumption-quiz/components/button";
 
 function ConsumptionQuiz()
 {
-    return <div className="flex flex-col justify-items-center">
-        <p className="text-5xl text-center my-20">Sprawdź czy wiesz, jakie urządzenia pobierają więcej energii elektrycznej</p>
+    return <div className="flex flex-col items-center">
+        <p className="text-5xl text-center my-20">Sprawdź czy wiesz, które urządzenia pobierają więcej energii elektrycznej</p>
 
         <StartButton />
     </div>
